5f0e66890932931fd3261cc84329193636dce97a
tracer-ics2015 <tracer@njuics.org>
2016-07-16 15:13:00 +0800

    >  compile NEMU 151220066 luyiming Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  15:13:00 up  6:54,  2 users,  load average: 0.08, 0.03, 0.04 c6df1e935a0b9b5562b96643c87e28aa49ccd40

--- nemu/src/cpu/exec/transfer/jcc.c
+++ nemu/src/cpu/exec/transfer/jcc.c
@@ -6,10 +6,11 @@
 
 make_helper(je_rel8) {
 	int8_t rel8 = instr_fetch(eip + 1, 1);
+    print_asm("je %x", (int32_t)cpu.eip + (int32_t)rel8 + 2);
+
     if(cpu.ZF == 1)
         cpu.eip = (int32_t)cpu.eip + (int32_t)rel8;
 
-	print_asm("je %x", (int32_t)cpu.eip + (int32_t)rel8 + 2);
 	return 2;
 }
 

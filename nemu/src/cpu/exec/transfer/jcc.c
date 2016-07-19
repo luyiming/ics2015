fa056a8ed40331894839c97d11b500146a097ace
tracer-ics2015 <tracer@njuics.org>
2016-07-19 20:07:38 +0800

    >  compile NEMU 151220066 luyiming Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  20:07:38 up  5:23,  2 users,  load average: 0.00, 0.01, 0.05 1794b74adfe492b2130a3f10d41b2f00e339646a

--- nemu/src/cpu/exec/transfer/jcc.c
+++ nemu/src/cpu/exec/transfer/jcc.c
@@ -1,6 +1,6 @@
 #include "cpu/exec/helper.h"
 
-make_helper(ja_rel8) {
+make_helper(ja_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("ja %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.CF == 0 && cpu.ZF == 0)
@@ -8,7 +8,7 @@ make_helper(ja_rel8) {
 	return 2;
 }
 
-make_helper(jae_rel8) {
+make_helper(jae_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jae %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.CF == 0 || cpu.ZF == 1)
@@ -16,7 +16,7 @@ make_helper(jae_rel8) {
 	return 2;
 }
 
-make_helper(jb_rel8) {
+make_helper(jb_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jb %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.CF == 1 && cpu.ZF == 0)
@@ -24,7 +24,7 @@ make_helper(jb_rel8) {
 	return 2;
 }
 
-make_helper(jbe_rel8) {
+make_helper(jbe_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jbe %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.CF == 1 || cpu.ZF == 1)
@@ -32,7 +32,7 @@ make_helper(jbe_rel8) {
 	return 2;
 }
 
-make_helper(je_rel8) {
+make_helper(je_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("je %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.ZF == 1)
@@ -40,7 +40,7 @@ make_helper(je_rel8) {
 	return 2;
 }
 
-make_helper(jne_rel8) {
+make_helper(jne_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jne %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.ZF == 0)
@@ -48,7 +48,7 @@ make_helper(jne_rel8) {
 	return 2;
 }
 
-make_helper(jg_rel8) {
+make_helper(jg_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jg %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.SF == cpu.OF && cpu.ZF == 0)
@@ -56,7 +56,7 @@ make_helper(jg_rel8) {
 	return 2;
 }
 
-make_helper(jge_rel8) {
+make_helper(jge_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jge %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.SF == cpu.OF || cpu.ZF == 1)
@@ -64,7 +64,7 @@ make_helper(jge_rel8) {
 	return 2;
 }
 
-make_helper(jl_rel8) {
+make_helper(jl_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jl %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.SF != cpu.OF && cpu.ZF == 0)
@@ -72,7 +72,7 @@ make_helper(jl_rel8) {
 	return 2;
 }
 
-make_helper(jle_rel8) {
+make_helper(jle_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jl %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.SF != cpu.OF || cpu.ZF == 1)
@@ -80,7 +80,7 @@ make_helper(jle_rel8) {
 	return 2;
 }
 
-make_helper(jno_rel8) {
+make_helper(jno_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jno %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.OF == 0)
@@ -88,7 +88,7 @@ make_helper(jno_rel8) {
 	return 2;
 }
 
-make_helper(jnp_rel8) {
+make_helper(jnp_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jnp %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.PF == 0)
@@ -96,7 +96,7 @@ make_helper(jnp_rel8) {
 	return 2;
 }
 
-make_helper(jns_rel8) {
+make_helper(jns_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jns %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.SF == 0)
@@ -104,7 +104,7 @@ make_helper(jns_rel8) {
 	return 2;
 }
 
-make_helper(jo_rel8) {
+make_helper(jo_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jo %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.OF == 1)
@@ -112,7 +112,7 @@ make_helper(jo_rel8) {
 	return 2;
 }
 
-make_helper(jp_rel8) {
+make_helper(jp_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("jp %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.PF == 1)
@@ -120,10 +120,381 @@ make_helper(jp_rel8) {
 	return 2;
 }
 
-make_helper(js_rel8) {
+make_helper(js_b) {
 	int8_t rel = instr_fetch(eip + 1, 1);
     print_asm("js %x", (int32_t)cpu.eip + (int32_t)rel + 2);
     if(cpu.SF == 1)
         cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
 	return 2;
+}
+
+/***************** disp 16 *********************/
+make_helper(ja_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("ja %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.CF == 0 && cpu.ZF == 0)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jae_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jae %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.CF == 0 || cpu.ZF == 1)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jb_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jb %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.CF == 1 && cpu.ZF == 0)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jbe_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jbe %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.CF == 1 || cpu.ZF == 1)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(je_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("je %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.ZF == 1)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jne_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jne %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.ZF == 0)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jg_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jg %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.SF == cpu.OF && cpu.ZF == 0)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jge_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jge %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.SF == cpu.OF || cpu.ZF == 1)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jl_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jl %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.SF != cpu.OF && cpu.ZF == 0)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jle_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jl %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.SF != cpu.OF || cpu.ZF == 1)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jno_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jno %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.OF == 0)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jnp_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jnp %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.PF == 0)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jns_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jns %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.SF == 0)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jo_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jo %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.OF == 1)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(jp_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("jp %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.PF == 1)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+make_helper(js_w) {
+	int16_t rel = instr_fetch(eip + 1, 2);
+    print_asm("js %x", (int32_t)cpu.eip + (int32_t)rel + 5);
+    if(cpu.SF == 1)
+        cpu.eip = (int32_t)cpu.eip + (int32_t)rel;
+	return 5; // prefix + opcode + disp
+}
+
+/***************** disp 32 *********************/
+make_helper(ja_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("ja %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.CF == 0 && cpu.ZF == 0)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jae_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jae %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.CF == 0 || cpu.ZF == 1)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jb_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jb %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.CF == 1 && cpu.ZF == 0)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jbe_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jbe %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.CF == 1 || cpu.ZF == 1)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(je_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("je %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.ZF == 1)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jne_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jne %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.ZF == 0)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jg_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jg %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.SF == cpu.OF && cpu.ZF == 0)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jge_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jge %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.SF == cpu.OF || cpu.ZF == 1)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jl_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jl %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.SF != cpu.OF && cpu.ZF == 0)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jle_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jl %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.SF != cpu.OF || cpu.ZF == 1)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jno_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jno %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.OF == 0)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jnp_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jnp %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.PF == 0)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jns_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jns %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.SF == 0)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jo_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jo %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.OF == 1)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(jp_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("jp %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.PF == 1)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+make_helper(js_l) {
+	int32_t rel = instr_fetch(eip + 1, 4);
+    print_asm("js %x", (int32_t)cpu.eip + rel + 6);
+    if(cpu.SF == 1)
+        cpu.eip = (int32_t)cpu.eip + rel;
+	return 6; // opcode + disp
+}
+
+/***************** helper v *********************/
+make_helper(ja_v) {
+    if(ops_decoded.is_operand_size_16)
+        return ja_w(eip);
+    else
+        return ja_l(eip);
+}
+
+make_helper(jae_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jae_w(eip);
+    else
+        return jae_l(eip);
+}
+
+make_helper(jb_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jb_w(eip);
+    else
+        return jb_l(eip);
+}
+
+make_helper(jbe_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jbe_w(eip);
+    else
+        return jbe_l(eip);
+}
+
+make_helper(je_v) {
+    if(ops_decoded.is_operand_size_16)
+        return je_w(eip);
+    else
+        return je_l(eip);
+}
+
+make_helper(jne_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jne_w(eip);
+    else
+        return jne_l(eip);
+}
+
+make_helper(jg_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jg_w(eip);
+    else
+        return jg_l(eip);
+}
+
+make_helper(jge_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jge_w(eip);
+    else
+        return jge_l(eip);
+}
+
+make_helper(jl_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jl_w(eip);
+    else
+        return jl_l(eip);
+}
+
+make_helper(jle_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jle_w(eip);
+    else
+        return jle_l(eip);
+}
+
+make_helper(jno_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jno_w(eip);
+    else
+        return jno_l(eip);
+}
+
+make_helper(jnp_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jnp_w(eip);
+    else
+        return jnp_l(eip);
+}
+
+make_helper(jns_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jns_w(eip);
+    else
+        return jns_l(eip);
+}
+
+make_helper(jo_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jo_w(eip);
+    else
+        return jo_l(eip);
+}
+
+make_helper(jp_v) {
+    if(ops_decoded.is_operand_size_16)
+        return jp_w(eip);
+    else
+        return jp_l(eip);
+}
+
+make_helper(js_v) {
+    if(ops_decoded.is_operand_size_16)
+        return js_w(eip);
+    else
+        return js_l(eip);
 }
--- nemu/src/cpu/exec/transfer/jcc.h
+++ nemu/src/cpu/exec/transfer/jcc.h
@@ -1,21 +1,37 @@
 #ifndef __JCC_H__
 #define __JCC_H__
 
-make_helper(ja_rel8);
-make_helper(jae_rel8);
-make_helper(jb_rel8);
-make_helper(jbe_rel8);
-make_helper(je_rel8);
-make_helper(jne_rel8);
-make_helper(jg_rel8);
-make_helper(jge_rel8);
-make_helper(jl_rel8);
-make_helper(jle_rel8);
-make_helper(jno_rel8);
-make_helper(jnp_rel8);
-make_helper(jns_rel8);
-make_helper(jo_rel8);
-make_helper(jp_rel8);
-make_helper(js_rel8);
+make_helper(ja_b);
+make_helper(jae_b);
+make_helper(jb_b);
+make_helper(jbe_b);
+make_helper(je_b);
+make_helper(jne_b);
+make_helper(jg_b);
+make_helper(jge_b);
+make_helper(jl_b);
+make_helper(jle_b);
+make_helper(jno_b);
+make_helper(jnp_b);
+make_helper(jns_b);
+make_helper(jo_b);
+make_helper(jp_b);
+make_helper(js_b);
 
+make_helper(ja_v);
+make_helper(jae_v);
+make_helper(jb_v);
+make_helper(jbe_v);
+make_helper(je_v);
+make_helper(jne_v);
+make_helper(jg_v);
+make_helper(jge_v);
+make_helper(jl_v);
+make_helper(jle_v);
+make_helper(jno_v);
+make_helper(jnp_v);
+make_helper(jns_v);
+make_helper(jo_v);
+make_helper(jp_v);
+make_helper(js_v);
 #endif
